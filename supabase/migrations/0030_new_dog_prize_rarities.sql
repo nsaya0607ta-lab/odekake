c1abbdf2c852fd1f718c8ee89dabe795859c76d3
nsaya0607ta-lab <n.saya0607.ta@gmail.com>
2026-08-14 15:57:57 +0900

    Add rarity mapping for six dog prizes

---
+++ supabase/migrations/0030_new_dog_prize_rarities.sql
@@ -0,0 +1,31 @@
+-- 新規図鑑/ガチャ景品のレア度を、DB側の重複返却判定へ追加する。
+create or replace function public.gacha_rarity_for_item(p_item_id text)
+returns text
+language sql
+immutable
+set search_path = public
+as $$
+  select case
+    when p_item_id in (
+      'toy_colorful_ball', 'toy_rope', 'toy_bone', 'toy_squeaky_ball',
+      'toy_tennis_ball', 'toy_red_slipper', 'toy_wood_stick', 'toy_donut_rope'
+    ) then 'N'
+    when p_item_id in (
+      'toy_duck_plush', 'toy_carrot', 'toy_frisbee', 'toy_soccer_ball',
+      'toy_taiyaki_plush', 'toy_bear_plush', 'food_paw_bowl'
+    ) then 'R'
+    when p_item_id in (
+      'toy_treasure_puzzle', 'toy_frenchie_plush', 'toy_meat', 'toy_frenchie_cushion'
+    ) then 'SR'
+    when p_item_id in (
+      'toy_rainbow_ball', 'toy_golden_crown_ball',
+      'hiking_frenchie', 'snow_frenchie', 'summer_frenchie',
+      'other_nakayoshi_azubee', 'other_kamunayo'
+    ) then 'SSR'
+    when p_item_id in (
+      'interior_anball', 'other_azubee', 'other_omojii',
+      'interior_kinoko_azubee', 'other_komochi', 'other_azuki', 'other_kobee'
+    ) then 'UR'
+    else null
+  end;
+$$;
